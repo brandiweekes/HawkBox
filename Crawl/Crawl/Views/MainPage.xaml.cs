fe381423e3395b723ea782358264444a3b7ba7d7
brandiweekes <brandiweekes@gmail.com>
2019-01-28 12:47:08 -0800

    changed app from tabbed page to masterDetail page; all menu items work except Home when clicked

--- Crawl/Crawl/Views/MainPage.xaml.cs
+++ Crawl/Crawl/Views/MainPage.xaml.cs
@@ -1,15 +1,62 @@
 ﻿
+using Crawl.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace Crawl.Views
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class MainPage : TabbedPage
+	public partial class MainPage : MasterDetailPage
 	{
+        Dictionary<int, NavigationPage> MenuPages = new Dictionary<int, NavigationPage>();
 		public MainPage ()
 		{
 			InitializeComponent ();
+
+            MasterBehavior = MasterBehavior.Popover;
+
+            MenuPages.Add((int)MenuItemType.Home, (NavigationPage)Detail);
 		}
+
+        public async Task NavigateFromMenu(int id)
+        {
+            switch (id)
+            {
+                case (int)MenuItemType.Home:
+                    MenuPages.Add(id, new NavigationPage(new HomePage()));
+                    break;
+                case (int)MenuItemType.Aliens:
+                    MenuPages.Add(id, new NavigationPage(new CharactersPage()));
+                    break;
+                case (int)MenuItemType.Agents:
+                    MenuPages.Add(id, new NavigationPage(new MonstersPage()));
+                    break;
+                case (int)MenuItemType.Items:
+                    MenuPages.Add(id, new NavigationPage(new ItemsPage()));
+                    break;
+                case (int)MenuItemType.Score:
+                    MenuPages.Add(id, new NavigationPage(new ScoresPage()));
+                    break;
+                case (int)MenuItemType.About:
+                    MenuPages.Add(id, new NavigationPage(new AboutPage()));
+                    break;
+            }
+
+            var newPage = MenuPages[id];
+
+            if (newPage != null && Detail != newPage)
+            {
+                Detail = newPage;
+
+                if(Device.RuntimePlatform == Device.Android)
+                {
+                    await Task.Delay(100);
+                }
+
+                IsPresented = false;
+            }
+        }
 	}
 }
