d5e98e243c5f4fe13906e6cd09e40285afff2f85
Karthikeyan Reddy Rokkam <rokkamkarthi@seattleu.edu>
2019-02-19 00:23:13 -0800

    Score Index page

--- Crawl/Crawl/Views/Scores/ScoresPage.xaml.cs
+++ Crawl/Crawl/Views/Scores/ScoresPage.xaml.cs
@@ -17,7 +17,44 @@ namespace Crawl.Views
         {
             InitializeComponent();
             BindingContext = _viewModel = ScoresViewModel.Instance;
+            _viewModel.Title = "Scores Page";
         }
 
+        private void AddScore(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new ScoreNewPage());
+        }
+
+        private async void OnScoreSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (!(e.SelectedItem is Score score))
+                return;
+
+            await Navigation.PushAsync(new ScoreDetailPage(new ScoreDetailViewModel(score)));
+
+            // Manually deselect item.
+            ScoresListView.SelectedItem = null;
+
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            BindingContext = null;
+
+            //reset toolbar for index page
+            if (ToolbarItems.Count > 0)
+                ToolbarItems.Clear();
+
+            //sets global binding
+            InitializeComponent();
+
+            // load or reload monsters on page
+            if (_viewModel.Dataset.Count == 0 || _viewModel.NeedsRefresh())
+                _viewModel.LoadScoresCommand.Execute(null);
+
+            BindingContext = _viewModel;
+        }
     }
 }
