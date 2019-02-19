2ec350a317c371cc64a9751793b8e3e51272c7bf
Karthikeyan Reddy Rokkam <rokkamkarthi@seattleu.edu>
2019-02-19 12:41:23 -0800

    Pick Characters page

---
+++ Crawl/Crawl/ViewModels/PickCharactersViewModel.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+using Crawl.Models;
+using Xamarin.Forms;
+
+namespace Crawl.ViewModels
+{
+    public class MultiSelectData : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public Character Data { get; set; }
+
+        private bool _IsSelected;
+        public bool IsSelected
+        {
+            get
+            {
+                return _IsSelected;
+            }
+            set
+            {
+                _IsSelected = value;
+                OnPropertyChanged("IsSelected");
+            }
+        }
+
+        private string _Image;
+        public string Image
+        {
+            get { return _Image; }
+            set
+            {
+                _Image = value;
+                OnPropertyChanged("Image");
+            }
+        }
+
+        public MultiSelectData(Character character, bool isSelected)
+        {
+            Data = character;
+            IsSelected = isSelected;
+            Image = IsSelected ? "checkbox_checked_icon.png" : "checkbox_unchecked_icon.png";
+        }
+
+        private void OnPropertyChanged(string props)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(props));
+        }
+
+    }
+
+    public class PickCharactersViewModel : BaseViewModel
+    {
+        #region Singleton
+        // Create one instance of view model
+        private static PickCharactersViewModel _instance;
+
+        public static PickCharactersViewModel Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new PickCharactersViewModel();
+                return _instance;
+            }
+        }
+        #endregion Singleton
+
+        public ObservableCollection<MultiSelectData> DataSet { get; set; }
+
+        public Command LoadCommand { get; set; }
+
+        private bool _needsRefresh;
+
+        public PickCharactersViewModel()
+        {
+            DataSet = new ObservableCollection<MultiSelectData>();
+            LoadCommand = new Command(async () => await ExecuteLoadCommand());
+        }
+
+        #region Refresh
+
+        // Return True if a refresh is needed
+        // It sets the refresh flag to false
+        public bool NeedsRefresh()
+        {
+            if (_needsRefresh)
+            {
+                _needsRefresh = false;
+                return true;
+            }
+
+            return false;
+
+        }
+
+        // Sets the need to refresh
+        public void SetNeedsRefresh(bool value)
+        {
+            _needsRefresh = value;
+        }
+
+        async Task ExecuteLoadCommand()
+        {
+            if (IsBusy)
+                return;
+
+            IsBusy = true;
+
+            try
+            {
+                DataSet.Clear();
+                var characters = await DataStore.GetAllAsync_Character(true);
+                foreach (var character in characters)
+                {
+                    DataSet.Add(new MultiSelectData(character, false));
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+            finally
+            {
+                IsBusy = false;
+                SetNeedsRefresh(false);
+            }
+        }
+
+        public void ForceDataRefresh()
+        {
+            LoadCommand.Execute(null);
+        }
+
+        #endregion Refresh
+    }
+}
---
+++ Crawl/Crawl/Views/Battle/PickCharactersPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using Crawl.ViewModels;
+using System;
+using System.Linq;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Crawl.Views.Battle
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PickCharactersPage : ContentPage
+    {
+        private PickCharactersViewModel _viewModel;
+
+        public PickCharactersPage()
+        {
+            InitializeComponent();
+            BindingContext = _viewModel = PickCharactersViewModel.Instance;
+            if (_viewModel.DataSet.Count == 0)
+                _viewModel.LoadCommand.Execute(null);
+        }
+
+        private void OnCharacterTapped(object sender, ItemTappedEventArgs e)
+        {
+            if (!(e.Item is MultiSelectData multiSelectData))
+                return;
+            var index = _viewModel.DataSet.IndexOf(multiSelectData);
+            _viewModel.DataSet[index].IsSelected = _viewModel.DataSet[index].IsSelected ? false : true;
+            _viewModel.DataSet[index].Image = _viewModel.DataSet[index].Image.Equals("checkbox_unchecked_icon.png")
+                ? "checkbox_checked_icon.png" : "checkbox_unchecked_icon.png";
+
+            CharactersListView.SelectedItem = null;
+            ValidateSelectedData();
+        }
+
+        private void NextClicked(object sender, EventArgs e)
+        {
+            Console.WriteLine(_viewModel);
+        }
+
+        private void ValidateSelectedData()
+        {
+            var dataset = _viewModel.DataSet.Where(s => s.IsSelected == true).ToList();
+            if (dataset.Count > 0 && dataset.Count < 7)
+                PickCharacters_NextButton.IsEnabled = true;
+            else
+                PickCharacters_NextButton.IsEnabled = false;
+        }
+    }
+}
