04896b3715a29ad12feba1265d5864aa8eb0e703
Karthikeyan Reddy Rokkam <rokkamkarthi@seattleu.edu>
2019-02-23 11:56:51 -0800

    Added Test Project to Hawkbox, Updated NUnit Nuget pkgs

---
+++ UnitTests/TestClass.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [Test]
+        public void TestMethod()
+        {
+            // TODO: Add your test code here
+            Assert.Pass("Your first passing test");
+
+            var Actual = 10;
+            var Expected = 10;
+
+            Assert.AreEqual(Expected, Actual);
+        }
+    }
+}
