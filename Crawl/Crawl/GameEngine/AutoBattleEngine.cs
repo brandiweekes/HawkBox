24f21b6cc3e4d5bbf23f4068c5c37590389b53b7
Karthikeyan Reddy Rokkam <rokkamkarthi@seattleu.edu>
2019-02-23 17:32:39 -0800

    Added unit tests

--- Crawl/Crawl/GameEngine/AutoBattleEngine.cs
+++ Crawl/Crawl/GameEngine/AutoBattleEngine.cs
@@ -1,5 +1,6 @@
 ﻿using Crawl.Models;
 using Crawl.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 
@@ -7,6 +8,14 @@ namespace Crawl.GameEngine
 {
     class AutoBattleEngine
     {
+        /// <summary>
+        /// Random should only be instantiated once
+        /// Because each call to new Random will reset the seed value, and thus the numbers generated
+        /// You can control the seed value for Random by passing a value to the constructor
+        /// Do that if you want to be able able get the same sequence of Random over and over
+        /// </summary>
+        private static Random rnd = new Random();
+
         public BattleEngine BattleEngine = new BattleEngine();
 
         // Start here...
@@ -51,12 +60,32 @@ namespace Crawl.GameEngine
 
         public List<Character> GetListOfCharacter(int number)
         {
+            // Number of characters cannot be less than zero or equal to zero.
+            if(number <= 0)
+            {
+                return null;
+            }
+
+            var _charactersDataset = CharactersViewModel.Instance.Dataset;
+
+            // if Dataset is not defined
+            if(_charactersDataset == null)
+            {
+                return null;
+            }
+            var _count = _charactersDataset.Count;
+
+            // No. of characters selected cannot be greater than dataset count
+            if(number > _count)
+            {
+                return null;
+            }
             var myReturn = new List<Character>();
 
-            // some call to Character DataStore to get characters
+            // Iterate for given number of times and fetch characters from dataset randomly.
             for (var i = 0; i < number; i++)
             {
-                myReturn.Add(CharactersViewModel.Instance.Dataset[i]);
+                myReturn.Add(_charactersDataset[rnd.Next(1, _count + 1)]);
             }
 
             return myReturn;
---
+++ UnitTests/GameEngineTests/AutoBattleEngineTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using Crawl.GameEngine;
+using Xamarin.Forms.Mocks;
+
+namespace UnitTests.GameEngineTests
+{
+    [TestFixture]
+    public class AutoBattleEngineTests
+    {
+
+        [Test]
+        public void GetListOfCharacter_Number_2_Should_Pass()
+        {
+            // Initialize Mock
+           MockForms.Init();
+
+            // Arrange
+            var Number = 2;
+            var Expected = 2;
+
+            // Act
+            var _list = new AutoBattleEngine().GetListOfCharacter(Number);
+            var Actual = _list.Count;
+
+            // Assert
+            Assert.AreEqual(Expected, Actual, TestContext.CurrentContext.Test.Name);
+        }
+    }
+}
