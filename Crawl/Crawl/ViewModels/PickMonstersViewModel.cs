0d4d5a3eabcee1cc581a3b2b2fd9df27c4795d95
Karthikeyan Reddy Rokkam <rokkamkarthi@seattleu.edu>
2019-03-04 19:50:25 -0800

    added pick monster view model..

---
+++ Crawl/Crawl/ViewModels/PickMonstersViewModel.cs
@@ -0,0 +1,37 @@
+﻿using Crawl.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Crawl.ViewModels
+{
+    public class PickMonstersViewModel : BaseViewModel
+    {
+        #region Singleton
+        // Create one instance of view model
+        private static PickMonstersViewModel _instance;
+
+        public static PickMonstersViewModel Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new PickMonstersViewModel();
+                return _instance;
+            }
+        }
+        #endregion Singleton
+
+        public ObservableCollection<Monster> DataSet { get; set; }
+
+        public PickMonstersViewModel()
+        {
+            DataSet = new ObservableCollection<Monster>();
+        }
+
+    }
+}
