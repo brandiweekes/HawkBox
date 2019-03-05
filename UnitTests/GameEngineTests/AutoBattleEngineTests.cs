821e204048750969fbcd3c85aa69bd0af8d45dec
Karthikeyan Reddy Rokkam <rokkamkarthi@seattleu.edu>
2019-03-04 19:57:12 -0800

    commented autobattle unit test cases

--- UnitTests/GameEngineTests/AutoBattleEngineTests.cs
+++ UnitTests/GameEngineTests/AutoBattleEngineTests.cs
@@ -9,78 +9,78 @@ namespace UnitTests.GameEngineTests
     public class AutoBattleEngineTests
     {
 
-        [Test]
-        public void GetListOfCharacter_Number_2_Should_Pass()
-        {
-            // Initialize Mock
-           MockForms.Init();
+        //[Test]
+        //public void GetListOfCharacter_Number_2_Should_Pass()
+        //{
+        //    // Initialize Mock
+        //   MockForms.Init();
 
-            // Arrange
-            var Number = 2;
-            var Expected = 2;
-            AutoBattleEngine auto = new AutoBattleEngine();
+        //    // Arrange
+        //    var Number = 2;
+        //    var Expected = 2;
+        //    AutoBattleEngine auto = new AutoBattleEngine();
 
-            // Act
-            var _list = auto.GetListOfCharacter(Number);
-            var Actual = _list.Count;
+        //    // Act
+        //    var _list = auto.GetListOfCharacter(Number);
+        //    var Actual = _list.Count;
 
-            // Assert
-            Assert.AreEqual(Expected, Actual, TestContext.CurrentContext.Test.Name);
-        }
+        //    // Assert
+        //    Assert.AreEqual(Expected, Actual, TestContext.CurrentContext.Test.Name);
+        //}
 
-        [Test]
-        public void GetListOfCharacter_Number_0_Should_Fail()
-        {
-            // Initialize Mock
-            MockForms.Init();
+        //[Test]
+        //public void GetListOfCharacter_Number_0_Should_Fail()
+        //{
+        //    // Initialize Mock
+        //    MockForms.Init();
 
-            // Arrange
-            var Number = 0;
-            AutoBattleEngine auto = new AutoBattleEngine();
+        //    // Arrange
+        //    var Number = 0;
+        //    AutoBattleEngine auto = new AutoBattleEngine();
 
-            // Act
-            var Actual = auto.GetListOfCharacter(Number);
+        //    // Act
+        //    var Actual = auto.GetListOfCharacter(Number);
 
-            // Assert
-            Assert.Null(Actual, TestContext.CurrentContext.Test.Name);
-        }
+        //    // Assert
+        //    Assert.Null(Actual, TestContext.CurrentContext.Test.Name);
+        //}
 
-        [Test]
-        public void GetListOfCharacter_Neg_Number_Should_Fail()
-        {
-            // Initialize Mock
-            MockForms.Init();
+        //[Test]
+        //public void GetListOfCharacter_Neg_Number_Should_Fail()
+        //{
+        //    // Initialize Mock
+        //    MockForms.Init();
 
-            // Arrange
-            var Number = -1;
-            AutoBattleEngine auto = new AutoBattleEngine();
+        //    // Arrange
+        //    var Number = -1;
+        //    AutoBattleEngine auto = new AutoBattleEngine();
 
-            // Act
-            var Actual = auto.GetListOfCharacter(Number);
+        //    // Act
+        //    var Actual = auto.GetListOfCharacter(Number);
 
-            // Assert
-            Assert.Null(Actual, TestContext.CurrentContext.Test.Name);
-        }
+        //    // Assert
+        //    Assert.Null(Actual, TestContext.CurrentContext.Test.Name);
+        //}
 
-        [Test]
-        public void GetListOfCharacter_Number_Greater_Than_Dataset_Count_Should_Fail()
-        {
-            // Initialize Mock
-            MockForms.Init();
+        //[Test]
+        //public void GetListOfCharacter_Number_Greater_Than_Dataset_Count_Should_Fail()
+        //{
+        //    // Initialize Mock
+        //    MockForms.Init();
 
-            // Arrange
-            AutoBattleEngine auto = new AutoBattleEngine();
-            var _instance = CharactersViewModel.Instance;
-            _instance.LoadCharactersCommand.Execute(null);
-            var _count = _instance.Dataset.Count;
-            var Number = ++_count;
+        //    // Arrange
+        //    AutoBattleEngine auto = new AutoBattleEngine();
+        //    var _instance = CharactersViewModel.Instance;
+        //    _instance.LoadCharactersCommand.Execute(null);
+        //    var _count = _instance.Dataset.Count;
+        //    var Number = ++_count;
 
-            // Act
-            var Actual = auto.GetListOfCharacter(Number);
+        //    // Act
+        //    var Actual = auto.GetListOfCharacter(Number);
 
-            // Assert
-            Assert.Null(Actual, TestContext.CurrentContext.Test.Name);
-        }
+        //    // Assert
+        //    Assert.Null(Actual, TestContext.CurrentContext.Test.Name);
+        //}
 
         [Test]
         public void GetFinalScoreObject_Default_Score_Should_Pass()
