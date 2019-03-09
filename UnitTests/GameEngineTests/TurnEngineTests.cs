4ab671f396a27904edb40d04b497151aca92e47d
brandiweekes <brandiweekes@gmail.com>
2019-03-09 13:46:57 -0800

    test case: attack choice returns lowest health monster

--- UnitTests/GameEngineTests/TurnEngineTests.cs
+++ UnitTests/GameEngineTests/TurnEngineTests.cs
@@ -41,5 +41,34 @@ namespace UnitTests.GameEngineTests
             // Assert
             Assert.IsNull(returnMonster, "Expected Monster choice: null");
         }
+
+        [Test]
+        public void TurnEngine_Character_AttackChoice_Monster_List_Should_Return_Lowest_Health()
+        {
+            // Arrange
+            var testTurnEngine = new TurnEngine();
+            var testCharacter = new Character();
+            testTurnEngine.MonsterList = new List<Monster>();
+            var lowHealthMonster = new Monster("Low Health", 
+                                                "monster low health", 
+                                                "", 1, 1, true, 
+                                                1, 1, 1, 10, 1, 
+                                                null, null, null, null, 
+                                                null, null, null);
+            var highHealthMonster = new Monster("High Health",
+                                                "monster high health",
+                                                "", 1, 1, true,
+                                                1, 1, 1, 10, 10,
+                                                null, null, null, null,
+                                                null, null, null);
+            testTurnEngine.MonsterList.Add(lowHealthMonster);
+            testTurnEngine.MonsterList.Add(highHealthMonster);
+
+            // Act
+            var returnMonster = testTurnEngine.AttackChoice(testCharacter);
+
+            // Assert
+            Assert.AreSame(lowHealthMonster, returnMonster, "Expected Monster choice: Low Health");
+        }
     }
 }
