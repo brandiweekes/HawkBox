fcbd233018c3e24115e044924c073a0269884610
Samuel Sandell <sandells@seattleu.edu>
2019-03-12 06:04:55 -0700

    round engine comments + more unit tests

--- Crawl/Crawl/GameEngine/RoundEngine.cs
+++ Crawl/Crawl/GameEngine/RoundEngine.cs
@@ -16,65 +16,84 @@ namespace Crawl.GameEngine
 
         // Player currently engaged
         public PlayerInfo PlayerCurrent;
-
+        //state of the round
         public RoundEnum RoundStateEnum = RoundEnum.Unknown; 
-
+        //constructor
         public RoundEngine()
         {
+            //clear item pool & monster list
             ClearLists();
         }
 
+        //clear lists
         private void ClearLists()
         {
+            //clear item pool
             ItemPool = new List<Item>();
+            //clear monster list
             MonsterList = new List<Monster>();
         }
 
         // Start the round, need to get the ItemPool, and Characters
         public void StartRound()
         {
+            //initialize round count to 0
             BattleScore.RoundCount = 0;
+            //call for a new round
             NewRound();
         }
 
         // Call to make a new set of monsters...
         public void NewRound()
         {
+            //end last round
             EndRound();
+            //add monsters to new round
             AddMonstersToRound();
+            //create player list of monsters and characters
             MakePlayerList();
+            //increment round count
             BattleScore.RoundCount++;
         }
 
+        //get average level of characters in character list
         public int GetAverageCharacterLevel()
         {
+            //if character list empty return 0
             if(CharacterList.Count < 1)
             {
                 return 0;
             }
 
+            //calculate avg & return
             var data = CharacterList.Average(m => m.Level);
             return (int)Math.Floor(data);
         }
 
+        //get minimum level of characters in character list
         public int GetMinCharacterLevel()
         {
+            //if character list empty return 0
             if (CharacterList.Count < 1)
             {
                 return 0;
             }
 
+            //get minimun & return
             var data = CharacterList.Min(m => m.Level);
             return data;
         }
 
+        //get maximum level of characters in character list
         public int GetMaxCharacterLevel()
         {
+            //if character list empty return 0
             if (CharacterList.Count < 1)
             {
                 return 0;
             }
 
+            //get maximum level & return
             var data = CharacterList.Max(m => m.Level);
             return data;
         }
@@ -97,6 +116,7 @@ namespace Crawl.GameEngine
             //if already monsters in db
             if(monsterModel.Dataset.Count() > 0)
             {
+                //scale values
                 var scalemin = 1;
                 var scalemax = 1;
                 var scaleavg = 1;
@@ -104,14 +124,18 @@ namespace Crawl.GameEngine
                 //get scale values
                 if (CharacterList.Any())
                 {
+                    //get minimum level
                     scalemin = GetMinCharacterLevel();
+                    //get max level
                     scalemax = GetMaxCharacterLevel();
+                    //get avg level
                     scaleavg = GetAverageCharacterLevel();
                 }
 
-                //scale monsters & add to list
+                //scale monsters & add to list while monsterlist count is less than 6
                 do
                 {
+                    //get random monster from monster viewmodel
                     var round = HelperEngine.RollDice(1, monsterModel.Dataset.Count());
                     {
                         //new monster
@@ -126,7 +150,7 @@ namespace Crawl.GameEngine
                 } while (MonsterList.Count < 6);
 
             }
-            else
+            else  //if no monsters in viewmodel
             {
                 //add 6 monsters
                 for(var i = 0; i < 6; i++)
@@ -163,50 +187,60 @@ namespace Crawl.GameEngine
         // RoundNextTurn
         public RoundEnum RoundNextTurn()
         {
+            //if no characters remaining
             if(CharacterList.Count < 1)
             {
+                //game is over, set round state enum & return
                 RoundStateEnum = RoundEnum.GameOver;
                 return RoundStateEnum;
             }
 
+            //if no monsters remaining 
             if(MonsterList.Count < 1)
             {
+                //call for a new round
                 return RoundEnum.NewRound;
             }
 
+            //get player whose turn it is
             PlayerCurrent = GetNextPlayerTurn();
-
+            //if current player is character
             if(PlayerCurrent.PlayerType == PlayerTypeEnum.Character)
             {
+                //character takes the turn
                 var player = CharacterList.Where(a => a.Guid == PlayerCurrent.Guid).FirstOrDefault();
                 TakeTurn(player);
             }
-            else if(PlayerCurrent.PlayerType == PlayerTypeEnum.Monster)
+            else if(PlayerCurrent.PlayerType == PlayerTypeEnum.Monster) //if current player a monster
             {
+                //monster takes the turn
                 var player = MonsterList.Where(a => a.Guid == PlayerCurrent.Guid).FirstOrDefault();
                 TakeTurn(player);
             }
 
+            //call for next turn
             RoundStateEnum = RoundEnum.NextTurn;
             return RoundStateEnum;
         }
 
+        //get the player next in line for a turn
         public PlayerInfo GetNextPlayerTurn()
         {
             // Recalculate Order
             OrderPlayerListByTurnOrder();
-
+            //call getnextplayer in list
             var PlayerCurrent = GetNextPlayerInList();
-
+            //return current player
             return PlayerCurrent;
         }
 
+        //order player list according to game rules
         public void OrderPlayerListByTurnOrder()
         {
             var myReturn = new List<PlayerInfo>();
-
+            //create player list
             MakePlayerList();
-
+            //order by: Speed -> Level -> XP -> Character or Monster -> Name -> order in list
             PlayerList = PlayerList.OrderByDescending(a => a.Speed)
                 .ThenByDescending(a => a.Level)
                 .ThenByDescending(a => a.ExperiencePoints)
@@ -216,64 +250,91 @@ namespace Crawl.GameEngine
                 .ToList();
         }
 
+        //create the player list for turns
         private void MakePlayerList()
         {
+            //initialize player list
             PlayerList = new List<PlayerInfo>();
+            //a player (character or monster)
             PlayerInfo player;
-
+            //var for player's order in list
             var listOrder = 0;
 
+            //go thru character list & add characters to player list
             foreach(var ch in CharacterList)
             {
+                //only add living characters
                 if (ch.Alive)
                 {
+                    //create playerinfo from character
                     player = new PlayerInfo(ch);
+                    //set list order
                     player.ListOrder = listOrder;
+                    //add to player list
                     PlayerList.Add(player);
+                    //increment list order
                     listOrder++;
                 }
             }
 
+            //go thru monster list & add monsters to player list
             foreach(var mon in MonsterList)
             {
+                //only add living monsters
                 if (mon.Alive)
                 {
+                    //new player from monster
                     player = new PlayerInfo(mon);
+                    //set list order
                     player.ListOrder = listOrder;
+                    //add to player list
                     PlayerList.Add(player);
+                    //increment list order
                     listOrder++;
                 }
             }
-
-
         }
 
+        //return next player in player list 
         public PlayerInfo GetNextPlayerInList()
         {
+            //check if player list is null
+            if (PlayerList == null)
+            {
+                return null;
+            }
+            
+            //if current player not set
             if(PlayerCurrent == null)
             {
+                //set current player to last player in list
                 PlayerCurrent = PlayerList.LastOrDefault();
             }
 
+            //look for current player in player list
             for(var i = 0; i < PlayerList.Count(); i++)
             {
+                //current player found
                 if(PlayerList[i].Guid == PlayerCurrent.Guid)
                 {
+                    //if current player not last in list
                     if(i < PlayerList.Count() - 1)
                     {
+                        //return next player
                         return PlayerList[i + 1];
                     }
-                    else
+                    else //if current player last in list
                     {
+                        //return first player in list
                         return PlayerList.FirstOrDefault();
                     }
                 }
             }
 
-
             return null;
         }
 
+        //equip character with items
         public void PickupItemsFromPool(Character character)
         {
             //if no items
@@ -292,6 +353,7 @@ namespace Crawl.GameEngine
             GetItemFromPoolIfBetter(character, ItemLocationEnum.Feet);
         }
 
+        //equip character with a better item if applicable in a given location
         public void GetItemFromPoolIfBetter(Character character, ItemLocationEnum setLocation)
         {
             //get items with location = setlocation
@@ -313,8 +375,10 @@ namespace Crawl.GameEngine
                 return;
             }
 
+            //go thru item pool looking for better item
             foreach(var item in itemList)
             {
+                //if the item is better than the current eqipped item, replace item
                 if(item.Value > currentItem.Value)
                 {
                     //add item to character
--- UnitTests/GameEngineTests/RoundEngineTests.cs
+++ UnitTests/GameEngineTests/RoundEngineTests.cs
@@ -45,6 +45,7 @@ namespace UnitTests.GameEngineTests
         [Test]
         public void Round_Engine_Constructor_Item_Pool_Count_Should_Equal_Zero()
         {
+            MockForms.Init();
 
             //arrange 
             RoundEngine roundEngine = new RoundEngine();
@@ -60,6 +61,7 @@ namespace UnitTests.GameEngineTests
         [Test]
         public void Round_Engine_Constructor_Monster_List_Count_Should_Equal_Zero()
         {
+            MockForms.Init();
 
             //arrange 
             RoundEngine roundEngine = new RoundEngine();
@@ -92,6 +94,8 @@ namespace UnitTests.GameEngineTests
         [Test] 
         public void Round_Engine_New_Round_Should_Increment_Round_Count()
         {
+            MockForms.Init();
+
             //arrange 
             RoundEngine roundEngine = new RoundEngine();
             var Expected = 1;
@@ -107,6 +111,8 @@ namespace UnitTests.GameEngineTests
         [Test]
         public void Round_Engine_New_Round_Monster_List_Count_Should_Equal_Six()
         {
+            MockForms.Init();
+
             //arrange 
             RoundEngine roundEngine = new RoundEngine();
             var Expected = 6;
@@ -122,6 +128,8 @@ namespace UnitTests.GameEngineTests
         [Test]
         public void Round_Engine_New_Round_Make_Player_List_Should_Pass()
         {
+            MockForms.Init();
+
             //arrange 
             RoundEngine roundEngine = new RoundEngine();
             var Expected = 6 + roundEngine.CharacterList.Count;
@@ -137,6 +145,8 @@ namespace UnitTests.GameEngineTests
         [Test]
         public void Round_Engine_New_Round_End_Round_Should_Pass()
         {
+            MockForms.Init();
+
             //arrange 
             RoundEngine roundEngine = new RoundEngine();
             var Expected = 0;
@@ -152,6 +162,8 @@ namespace UnitTests.GameEngineTests
         [Test]
         public void Round_Engine_Get_Average_Character_Level_Should_Pass()
         {
+            MockForms.Init();
+
             //arrange 
             RoundEngine roundEngine = new RoundEngine();
             roundEngine.CharacterList.Clear();
@@ -176,6 +188,8 @@ namespace UnitTests.GameEngineTests
         [Test]
         public void Round_Engine_Get_Average_Character_Empty_Character_List_Should_Return_Zero()
         {
+            MockForms.Init();
+
             //arrange 
             RoundEngine roundEngine = new RoundEngine();
             roundEngine.CharacterList.Clear();
@@ -192,6 +206,8 @@ namespace UnitTests.GameEngineTests
         [Test]
         public void Round_Engine_Get_Min_Character_Level_Should_Pass()
         {
+            MockForms.Init();
+
             //arrange 
             RoundEngine roundEngine = new RoundEngine();
             roundEngine.CharacterList.Clear();
@@ -216,6 +232,8 @@ namespace UnitTests.GameEngineTests
         [Test]
         public void Round_Engine_Get_Min_Character_Empty_Character_List_Should_Return_Zero()
         {
+            MockForms.Init();
+
             //arrange 
             RoundEngine roundEngine = new RoundEngine();
             roundEngine.CharacterList.Clear();
@@ -232,6 +250,8 @@ namespace UnitTests.GameEngineTests
         [Test]
         public void Round_Engine_Get_Max_Character_Level_Should_Pass()
         {
+            MockForms.Init();
+
             //arrange 
             RoundEngine roundEngine = new RoundEngine();
             roundEngine.CharacterList.Clear();
@@ -256,6 +276,8 @@ namespace UnitTests.GameEngineTests
         [Test]
         public void Round_Engine_Get_Max_Character_Empty_Character_List_Should_Return_Zero()
         {
+            MockForms.Init();
+
             //arrange 
             RoundEngine roundEngine = new RoundEngine();
             roundEngine.CharacterList.Clear();
@@ -272,6 +294,8 @@ namespace UnitTests.GameEngineTests
         [Test]
         public void Round_Engine_Round_Next_Turn_No_Characters_Should_Return_Game_Over()
         {
+            MockForms.Init();
+
             //arrange 
             RoundEngine roundEngine = new RoundEngine();
             roundEngine.CharacterList.Clear();
@@ -288,6 +312,8 @@ namespace UnitTests.GameEngineTests
         [Test]
         public void Round_Engine_Round_Next_Turn_No_Monsters_Should_Return_New_Round()
         {
+            MockForms.Init();
+
             //arrange 
             RoundEngine roundEngine = new RoundEngine();
             roundEngine.CharacterList.Add(new Character());
@@ -305,6 +331,8 @@ namespace UnitTests.GameEngineTests
         [Test]
         public void Round_Engine_Round_Next_Turn_Two_Characters_Two_Monsters_Should_Return_Next_Turn()
         {
+            MockForms.Init();
+
             //arrange 
             RoundEngine roundEngine = new RoundEngine();
             roundEngine.CharacterList.Clear();
@@ -321,6 +349,189 @@ namespace UnitTests.GameEngineTests
 
             //assert
             Assert.AreEqual(Expected, Actual, TestContext.CurrentContext.Test.Name);
+        }
+
+        [Test]
+        public void Round_Engine_Get_Next_Player_In_List_Player_Current_Null_Player_List_Null_Should_Return_Null()
+        {
+            MockForms.Init();
+
+            //arrange 
+            RoundEngine roundEngine = new RoundEngine();
+            roundEngine.PlayerCurrent = null;
+            roundEngine.PlayerList = null;
+
+            //act 
+            var Actual = roundEngine.GetNextPlayerInList();
+            //reset
+
+            //assert
+            Assert.AreEqual(null, Actual, TestContext.CurrentContext.Test.Name);
+        }
+
+        [Test]
+        public void Round_Engine_Get_Next_Player_In_List_Player_Current_Null_Should_Return_First_Player_In_Player_List()
+        {
+            MockForms.Init();
+
+            //arrange 
+            RoundEngine roundEngine = new RoundEngine();
+            roundEngine.PlayerCurrent = null;
+            roundEngine.PlayerList = new List<PlayerInfo>();
+            roundEngine.PlayerList.Clear();
+            roundEngine.PlayerList.Add(new PlayerInfo(new Character()));
+            roundEngine.PlayerList.Add(new PlayerInfo(new Character()));
+            roundEngine.PlayerList.Add(new PlayerInfo(new Monster()));
+            roundEngine.PlayerList.Add(new PlayerInfo(new Monster()));
+            var Expected = roundEngine.PlayerList[0].Guid;
+
+            //act 
+            var Actual = roundEngine.GetNextPlayerInList().Guid;
+            
+            //reset
+
+            //assert
+            Assert.AreEqual(Expected, Actual, TestContext.CurrentContext.Test.Name);
+        }
+
+        [Test]
+        public void Round_Engine_Get_Next_Player_In_List_Player_Current_In_Middle_Of_List_Should_Return_Next_Player_In_Player_List()
+        {
+            MockForms.Init();
+
+            //arrange 
+            RoundEngine roundEngine = new RoundEngine();
+            roundEngine.PlayerList = new List<PlayerInfo>();
+            roundEngine.PlayerList.Clear();
+            roundEngine.PlayerList.Add(new PlayerInfo(new Character()));
+            roundEngine.PlayerList.Add(new PlayerInfo(new Character()));
+            roundEngine.PlayerList.Add(new PlayerInfo(new Monster()));
+            roundEngine.PlayerList.Add(new PlayerInfo(new Monster()));
+            roundEngine.PlayerList.Add(new PlayerInfo(new Character()));
+            roundEngine.PlayerList.Add(new PlayerInfo(new Monster()));
+
+            roundEngine.PlayerCurrent = roundEngine.PlayerList[3];
+            var Expected = roundEngine.PlayerList[4].Guid;
+
+            //act 
+            var Actual = roundEngine.GetNextPlayerInList().Guid;
+
+            //reset
+
+            //assert
+            Assert.AreEqual(Expected, Actual, TestContext.CurrentContext.Test.Name);
+        }
+
+        [Test]
+        public void Round_Engine_Get_Next_Player_In_List_Player_Current_At_End_Of_List_Should_Return_First_Player_In_Player_List()
+        {
+            MockForms.Init();
+
+            //arrange 
+            RoundEngine roundEngine = new RoundEngine();
+            roundEngine.PlayerList = new List<PlayerInfo>();
+            roundEngine.PlayerList.Clear();
+            roundEngine.PlayerList.Add(new PlayerInfo(new Character()));
+            roundEngine.PlayerList.Add(new PlayerInfo(new Character()));
+            roundEngine.PlayerList.Add(new PlayerInfo(new Monster()));
+            roundEngine.PlayerList.Add(new PlayerInfo(new Monster()));
+            roundEngine.PlayerList.Add(new PlayerInfo(new Character()));
+            roundEngine.PlayerList.Add(new PlayerInfo(new Monster()));
+
+            roundEngine.PlayerCurrent = roundEngine.PlayerList[5];
+            var Expected = roundEngine.PlayerList[0].Guid;
+
+            //act 
+            var Actual = roundEngine.GetNextPlayerInList().Guid;
+
+            //reset
+
+            //assert
+            Assert.AreEqual(Expected, Actual, TestContext.CurrentContext.Test.Name);
+        }
+
+        [Test]
+        public void Round_Engine_Get_Next_Player_In_List_Player_Current_Not_In_List_Should_Return_Null()
+        {
+            MockForms.Init();
+
+            //arrange 
+            RoundEngine roundEngine = new RoundEngine();
+            roundEngine.PlayerList = new List<PlayerInfo>();
+            roundEngine.PlayerList.Clear();
+            roundEngine.PlayerList.Add(new PlayerInfo(new Character()));
+            roundEngine.PlayerList.Add(new PlayerInfo(new Character()));
+            roundEngine.PlayerList.Add(new PlayerInfo(new Monster()));
+            roundEngine.PlayerList.Add(new PlayerInfo(new Monster()));
+            roundEngine.PlayerList.Add(new PlayerInfo(new Character()));
+            roundEngine.PlayerList.Add(new PlayerInfo(new Monster()));
+
+            roundEngine.PlayerCurrent = new PlayerInfo(new Character());
+
+            //act 
+            var Actual = roundEngine.GetNextPlayerInList();
+
+            //reset
+
+            //assert
+            Assert.AreEqual(null, Actual, TestContext.CurrentContext.Test.Name);
+        }
+
+        [Test]
+        public void Round_Engine_Order_Player_List_By_Turn_Order_Order_By_Speed_Should_Pass()
+        {
+            MockForms.Init();
+
+            //arrange
+            RoundEngine roundEngine = new RoundEngine();
+            Character char1 = new Character();
+            char1.Attribute.Speed = 4;
+            Character char2 = new Character();
+            char2.Attribute.Speed = 6;
+            Character char3 = new Character();
+            char3.Attribute.Speed = 2;
+            Monster mon1 = new Monster();
+            mon1.Attribute.Speed = 3;
+            Monster mon2 = new Monster();
+            mon2.Attribute.Speed = 1;
+            Monster mon3 = new Monster();
+            mon3.Attribute.Speed = 5;
+
+            //roundEngine.PlayerList = new List<PlayerInfo>();
+            roundEngine.CharacterList.Add(char1);
+            roundEngine.CharacterList.Add(char2);
+            roundEngine.CharacterList.Add(char3);
+            roundEngine.MonsterList.Add(mon1);
+            roundEngine.MonsterList.Add(mon2);
+            roundEngine.MonsterList.Add(mon3);
+
+
+            var ex1 = 6;
+            var ex2 = 5;
+            var ex3 = 4;
+            var ex4 = 3;
+            var ex5 = 2;
+            var ex6 = 1;
+
+
+            //act
+            roundEngine.OrderPlayerListByTurnOrder();
+            var actual1 = roundEngine.PlayerList[0].Speed;
+            var actual2 = roundEngine.PlayerList[1].Speed;
+            var actual3 = roundEngine.PlayerList[2].Speed;
+            var actual4 = roundEngine.PlayerList[3].Speed;
+            var actual5 = roundEngine.PlayerList[4].Speed;
+            var actual6 = roundEngine.PlayerList[5].Speed;
+
+            //reset
+            //assert
+            Assert.AreEqual(ex1, actual1, TestContext.CurrentContext.Test.Name);
+            Assert.AreEqual(ex2, actual2, TestContext.CurrentContext.Test.Name);
+            Assert.AreEqual(ex3, actual3, TestContext.CurrentContext.Test.Name);
+            Assert.AreEqual(ex4, actual4, TestContext.CurrentContext.Test.Name);
+            Assert.AreEqual(ex5, actual5, TestContext.CurrentContext.Test.Name);
+            Assert.AreEqual(ex6, actual6, TestContext.CurrentContext.Test.Name);
+
         }
     }
 }
