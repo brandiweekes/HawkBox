a0f9312ef69737c428437ba8ae0b8bd34fbaef14
brandiweekes <brandiweekes@gmail.com>
2019-03-19 15:58:57 -0700

    getting images from lists and setting to battle screen character box; monster ready to go too

--- Crawl/Crawl/Views/Battle/BattleRoundsPage.xaml.cs
+++ Crawl/Crawl/Views/Battle/BattleRoundsPage.xaml.cs
@@ -26,8 +26,19 @@ namespace Crawl.Views.Battle
 
             InitializeComponent();
 
+            _viewModel.StartBattle();
+
+
             // Load the Characters into the Battle Engine
             _viewModel.LoadCharacters();
+
+
+            _viewModel.StartRound();
+
+
+            BindingContext = _viewModel;
+
+            //GoToModalPageMonsterList();
 
             PositionPlayersOnScreen();
         }
@@ -43,24 +54,71 @@ namespace Crawl.Views.Battle
 
         private void AddMonstersToBox(RelativeLayout MonstersRelativeLayout)
         {
+            
+            int index = 0;
+
+            Image monster1 = MonstersRelativeLayout.FindByName<Image>("mons1");
+            Image monster2 = MonstersRelativeLayout.FindByName<Image>("mons2");
+            Image monster3 = MonstersRelativeLayout.FindByName<Image>("mons3");
+            Image monster4 = MonstersRelativeLayout.FindByName<Image>("mons4");
+            Image monster5 = MonstersRelativeLayout.FindByName<Image>("mons5");
+            Image monster6 = MonstersRelativeLayout.FindByName<Image>("mons6");
+
+            List<Image> monsterImages = new List<Image>()
+            {
+                monster1, monster2, monster3, monster4, monster5, monster6
+            };
+
+            foreach (Image image in monsterImages)
+            {
+                image.Source = null;
+            }
+
+            List<Uri> imageURIs = new List<Uri>();
+
+            foreach (var monster in this._viewModel.BattleEngine.MonsterList)
+            {
+                monsterImages[index++].Source = new Uri(monster.ImageURI);
+            }
+
+            //TODO: remove this when have monster list 
             MonstersRelativeLayout.FindByName<Image>("mons1").Source = new Uri(HawkboxResources.Monsters_Female_Agent_A);
             MonstersRelativeLayout.FindByName<Image>("mons2").Source = new Uri(HawkboxResources.Monsters_Male_Agent_B);
             MonstersRelativeLayout.FindByName<Image>("mons3").Source = new Uri(HawkboxResources.Monsters_Female_Agent_B);
             MonstersRelativeLayout.FindByName<Image>("mons4").Source = new Uri(HawkboxResources.Monsters_Male_Agent_B);
             MonstersRelativeLayout.FindByName<Image>("mons5").Source = null;
             MonstersRelativeLayout.FindByName<Image>("mons6").Source = new Uri(HawkboxResources.Monsters_Female_Agent_E);
+
         }
 
         private void AddCharactersToBox(RelativeLayout CharactersRelativeLayout)
         {
-            
-            CharactersRelativeLayout.FindByName<Image>("char1").Source = new Uri(HawkboxResources.Aliens_Char_7);
-            CharactersRelativeLayout.FindByName<Image>("char2").Source = new Uri(HawkboxResources.Aliens_Char_5);
-            CharactersRelativeLayout.FindByName<Image>("char3").Source = new Uri(HawkboxResources.Aliens_Char_3);
-            CharactersRelativeLayout.FindByName<Image>("char4").Source = new Uri(HawkboxResources.Aliens_Char_1);
-            CharactersRelativeLayout.FindByName<Image>("char5").Source = null;
-            CharactersRelativeLayout.FindByName<Image>("char6").Source = new Uri(HawkboxResources.Aliens_Char_6);
+            int index = 0;
 
+            Image character1 = CharactersRelativeLayout.FindByName<Image>("char1");
+            Image character2 = CharactersRelativeLayout.FindByName<Image>("char2");
+            Image character3 = CharactersRelativeLayout.FindByName<Image>("char3");
+            Image character4 = CharactersRelativeLayout.FindByName<Image>("char4");
+            Image character5 = CharactersRelativeLayout.FindByName<Image>("char5");
+            Image character6 = CharactersRelativeLayout.FindByName<Image>("char6");
+
+            List<Image> characterImages = new List<Image>()
+            {
+                character1, character2, character3, character4, character5, character6
+            };
+
+            foreach(Image image in characterImages)
+            {
+                image.Source = null;
+            }
+
+            List<Uri> imageURIs = new List<Uri>();
+
+            foreach(var character in this._viewModel.BattleEngine.CharacterList)
+            {
+                characterImages[index++].Source = new Uri(character.ImageURI);
+            }
+            
         }
 
         public async void ItemPagesClicked(object sender, EventArgs e)
