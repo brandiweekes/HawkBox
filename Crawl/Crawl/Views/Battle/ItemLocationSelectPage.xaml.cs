f7d40407a3b3d0e1d0703e0dfe14c92d6b4fa248
Samuel Sandell <sandells@seattleu.edu>
2019-03-21 18:46:39 -0700

    item pages comments

--- Crawl/Crawl/Views/Battle/ItemLocationSelectPage.xaml.cs
+++ Crawl/Crawl/Views/Battle/ItemLocationSelectPage.xaml.cs
@@ -15,8 +15,12 @@ namespace Crawl.Views.Battle
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class ItemLocationSelectPage : ContentPage
 	{
+        //battleviewmodel instance holding game information
         BattleViewModel viewModel;
 
+        /// <summary>
+        /// Constructor
+        /// </summary>
 		public ItemLocationSelectPage ()
 		{
 			InitializeComponent();
@@ -32,6 +36,7 @@ namespace Crawl.Views.Battle
                 //add remaining characters to remaining character observable collection in BattleViewModel
                 foreach (var ch in BattleViewModel.Instance.BattleEngine.CharacterList)
                 {
+                    //add remaining character
                     BattleViewModel.Instance.RemainingCharacters.Add(ch);
                     Debug.WriteLine(ch.Name);
                 }
@@ -41,142 +46,189 @@ namespace Crawl.Views.Battle
             BindingContext = viewModel = BattleViewModel.Instance;
 		}
 
+        /// <summary>
+        /// button handler, takes user to next round
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private async void NextButtonClicked(object sender, EventArgs e)
         {
+            //pop back to battleroundspage
             await Navigation.PopModalAsync();
         }
 
+        /// <summary>
+        /// Event handler to select item for selected location
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private async void LocationClicked(object sender, EventArgs e)
         {
+            //get location of button clicked
             var button = (Button)sender;
             ItemLocationEnum buttonEnum = (ItemLocationEnum)Int32.Parse(button.ClassId);
+            //clear viewmodel available items observablecollection
             viewModel.AvailableItems.Clear();
-
+            //get available items at location
             var itemList = viewModel.BattleEngine.ItemPool.Where(a => a?.Location == buttonEnum).OrderByDescending(a => a.Value).ToList();
-
+            //add items to available items
             foreach(var item in itemList)
             {
                 viewModel.AvailableItems.Add(item);
             }
 
-
+            //if no character selected
             if(viewModel.pickedCharacter == null)
             {
+                //give instructions
                 InstructionLabel.Text = String.Format("Select a character");
             }
+            //if no items available and no items equipped
             else if (viewModel.pickedCharacter.GetItemByLocation(buttonEnum) == null && itemList.Count < 1 )
             {
+                //display message to user
                 InstructionLabel.Text = String.Format("No items available for this location");
             }
+            //available items or item currently equipped in location
             else
             {
+                //deselect character
                 RemainingListView.SelectedItem = null;
-
+                //set labels to default
                 AttackLabel.Text = String.Format("--");
                 DefenseLabel.Text = String.Format("--");
                 SpeedLabel.Text = String.Format("--");
                 HPLabel.Text = String.Format("--");
                 XPLabel.Text = String.Format("--");
-
+                //push item select page
                 await Navigation.PushModalAsync(new ItemSelectPage(buttonEnum));
             }
 
         }
         
+        /// <summary>
+        /// Event handler for remaining character listview
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
         private void OnCharacterSelected(object sender, SelectedItemChangedEventArgs args)
         {
+            //if null, return
             if (!(args.SelectedItem is Character character)) {
                 return;
             }
-
+            //set picked character in battleviewmodel
             viewModel.pickedCharacter = character;
-
+            //set attribute labels
             AttackLabel.Text = String.Format("{0}", character.Attribute.Attack + character.GetItemBonus(AttributeEnum.Attack));
             DefenseLabel.Text = String.Format("{0}", character.Attribute.Defense + character.GetItemBonus(AttributeEnum.Defense));
             SpeedLabel.Text = String.Format("{0}", character.Attribute.Speed + character.GetItemBonus(AttributeEnum.Speed));
             HPLabel.Text = String.Format("{0}", character.Attribute.CurrentHealth);
-            XPLabel.Text = String.Format("{0}", character.ExperienceTotal);
+            XPLabel.Text = String.Format("{0}", character.ExperienceTotal);         
 
+            //show items picked characters current items
+
+            //get and display head item
             var ItemInLocation = viewModel.pickedCharacter.GetItemByLocation(ItemLocationEnum.Head);
             if(ItemInLocation != null)
             {
+                //if not null, display item image & name on the button
                 HeadImage.Source = ItemInLocation.ImageURI;
                 HeadButton.Text = String.Format(ItemInLocation.Name);
             }
             else
             {
+                //if no item, default label & image
                 HeadImage.Source = "https://screenshotlayer.com/images/assets/placeholder.png";
                 HeadButton.Text = String.Format("None");
             }
 
+            //get and display necklace item
             ItemInLocation = viewModel.pickedCharacter.GetItemByLocation(ItemLocationEnum.Necklass);
             if (ItemInLocation != null)
             {
+                //if not null, set item image & name
                 NecklaceImage.Source = ItemInLocation.ImageURI;
                 NecklaceButton.Text = String.Format(ItemInLocation.Name);
             }
             else
             {
+                //if no item, default img and label
                 NecklaceImage.Source = "https://screenshotlayer.com/images/assets/placeholder.png";
                 NecklaceButton.Text = String.Format("None");
             }
 
+            //offhand
             ItemInLocation = viewModel.pickedCharacter.GetItemByLocation(ItemLocationEnum.OffHand);
             if (ItemInLocation != null)
             {
+                //item img & name
                 OHImage.Source = ItemInLocation.ImageURI;
                 OHButton.Text = String.Format(ItemInLocation.Name);
             }
             else
             {
+                //default img and label
                 OHImage.Source = "https://screenshotlayer.com/images/assets/placeholder.png";
                 OHButton.Text = String.Format("None");
             }
 
+            //priamryhand
             ItemInLocation = viewModel.pickedCharacter.GetItemByLocation(ItemLocationEnum.PrimaryHand);
             if (ItemInLocation != null)
             {
+                //item img and name
                 PHImage.Source = ItemInLocation.ImageURI;
                 PHButton.Text = String.Format(ItemInLocation.Name);
             }
             else
             {
+                //default img and label
                 PHImage.Source = "https://screenshotlayer.com/images/assets/placeholder.png";
                 PHButton.Text = String.Format("None");
             }
 
+            //left finger
             ItemInLocation = viewModel.pickedCharacter.GetItemByLocation(ItemLocationEnum.LeftFinger);
             if (ItemInLocation != null)
             {
+                //item img and name
                 LFImage.Source = ItemInLocation.ImageURI;
                 LFButton.Text = String.Format(ItemInLocation.Name);
             }
             else
             {
+                //default img and label
                 LFImage.Source = "https://screenshotlayer.com/images/assets/placeholder.png";
                 LFButton.Text = String.Format("None");
             }
 
+            //right finger
             ItemInLocation = viewModel.pickedCharacter.GetItemByLocation(ItemLocationEnum.RightFinger);
             if (ItemInLocation != null)
             {
+                //item image and name
                 RFImage.Source = ItemInLocation.ImageURI;
                 RFButton.Text = String.Format(ItemInLocation.Name);
             }
             else
             {
+                //default image and label
                 RFImage.Source = "https://screenshotlayer.com/images/assets/placeholder.png";
                 RFButton.Text = String.Format("None");
             }
 
+            //feet
             ItemInLocation = viewModel.pickedCharacter.GetItemByLocation(ItemLocationEnum.Feet);
             if (ItemInLocation != null)
             {
+                //item img and name
                 FeetImage.Source = ItemInLocation.ImageURI;
                 FeetButton.Text = String.Format(ItemInLocation.Name);
             }
             else
             {
+                //default img and label
                 FeetImage.Source = "https://screenshotlayer.com/images/assets/placeholder.png";
                 FeetButton.Text = String.Format("None");
             }
--- Crawl/Crawl/Views/Battle/ItemSelectPage.xaml.cs
+++ Crawl/Crawl/Views/Battle/ItemSelectPage.xaml.cs
@@ -14,45 +14,61 @@ namespace Crawl.Views.Battle
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class ItemSelectPage : ContentPage
 	{
+        //battleviewmodel instance
         BattleViewModel viewModel;
+        //location
         ItemLocationEnum location;
+        //currently equipped item
         Item EquippedItem;
+        //selected item in listview
         Item SelectedItem;
 
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="loc"></param>
 		public ItemSelectPage (ItemLocationEnum loc)
 		{
 			InitializeComponent ();
-
+            //set binding context
             BindingContext = viewModel = BattleViewModel.Instance;
-
+            //set location
             location = loc;
+            //set equipped item
             EquippedItem = viewModel.pickedCharacter.GetItemByLocation(location);
+            //set selected item to null
             SelectedItem = null;
 
+            //set image of equipped item to default if no item equipped
             if(EquippedItem != null)
             {
                 ItemPic.Source = EquippedItem.ImageURI;
             }
 
-
+            //set labels for character attributes and image
             CharPic.Source = viewModel.pickedCharacter.ImageURI;
             AttackLabel.Text = String.Format("{0}", viewModel.pickedCharacter.Attribute.Attack + viewModel.pickedCharacter.GetItemBonus(AttributeEnum.Attack));
             DefenseLabel.Text = String.Format("{0}", viewModel.pickedCharacter.Attribute.Defense + viewModel.pickedCharacter.GetItemBonus(AttributeEnum.Defense));
             SpeedLabel.Text = String.Format("{0}", viewModel.pickedCharacter.Attribute.Speed + viewModel.pickedCharacter.GetItemBonus(AttributeEnum.Speed));
             HPLabel.Text = String.Format("{0}", viewModel.pickedCharacter.Attribute.CurrentHealth);
             XPLabel.Text = String.Format("{0}", viewModel.pickedCharacter.ExperienceTotal);
-
+            //give instructions
             ItemDescLabel.Text = "To add an item, press Equip then Save\nTo remove item, press Unequip and Save\n" +
                    "To discard changes, press Cancel\nSave must be pressed to finalize equipped or unequipped items.";
         }
 
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="vm"></param>
         public ItemSelectPage(BattleViewModel vm)
         {
             InitializeComponent();
 
+            //binding context
             BindingContext = viewModel = vm;
 
-
+            //set labels
             CharPic.Source = BattleViewModel.Instance.pickedCharacter.ImageURI;
             AttackLabel.Text = String.Format("{0}", BattleViewModel.Instance.pickedCharacter.Attribute.Attack 
                 + BattleViewModel.Instance.pickedCharacter.GetItemBonus(AttributeEnum.Attack));
@@ -61,12 +77,14 @@ namespace Crawl.Views.Battle
             SpeedLabel.Text = String.Format("{0}", BattleViewModel.Instance.pickedCharacter.Attribute.Speed 
                 + BattleViewModel.Instance.pickedCharacter.GetItemBonus(AttributeEnum.Speed));
             HPLabel.Text = String.Format("{0}", BattleViewModel.Instance.pickedCharacter.Attribute.CurrentHealth);
-            XPLabel.Text = String.Format("{0}", BattleViewModel.Instance.pickedCharacter.ExperienceTotal);
-
-            //ItemDescLabel.Text = "Select Item";
-            //ItemAffectsLabel.Text = "Select Item";
+            XPLabel.Text = String.Format("{0}", BattleViewModel.Instance.pickedCharacter.ExperienceTotal);          
         }
 
+        /// <summary>
+        /// Save button handler, finalizes changes
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         public async void SaveButtonClicked(object sender, EventArgs e)
         {
             //cases
@@ -114,69 +132,105 @@ namespace Crawl.Views.Battle
 
 
             //need to update character in character list to reflect changes in equipment
-            //viewModel.BattleEngine.CharacterList.Remove(viewModel.pickedCharacter);
             viewModel.BattleEngine.CharacterList[viewModel.BattleEngine.CharacterList.FindIndex(ch => ch.Id.Equals(viewModel.pickedCharacter.Id))] = viewModel.pickedCharacter;
-
+            //set picked character null
             viewModel.pickedCharacter = null;
+            //clear available items
             viewModel.AvailableItems.Clear();
-
+            //pop back to itemlocaionselectpage
             await Navigation.PopModalAsync();
-            //await Navigation.PushModalAsync(new ItemLocationSelectPage());
         }
 
+        /// <summary>
+        /// cancel button handler, cancels changes
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         public async void CancelButtonClicked(object sender, EventArgs e)
         {
+            //set pickedcharacter to null and clear available items list
             viewModel.pickedCharacter = null;
             viewModel.AvailableItems.Clear();
-           
+            //pop page
             await Navigation.PopModalAsync();
         }
 
+        /// <summary>
+        ///Equipped button handler, equips item, preparing it to be added to character upon save button click 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         public void EquipButtonClicked(object sender, EventArgs e)
         {
+            //if no selected item
             if(SelectedItem == null)
             {
                 return;
             }
 
+            //if no current item
             if (EquippedItem == null)
             {
+                //set equipped item
                 EquippedItem = SelectedItem;
+                //remove selected from available items
                 viewModel.AvailableItems.Remove(SelectedItem);
+                //set image
                 ItemPic.Source = EquippedItem.ImageURI;
             }
+            //if current item in slot
             else
             {
+                //add current item to available items
                 viewModel.AvailableItems.Add(EquippedItem);
+                //set equipped item
                 EquippedItem = SelectedItem;
+                //remove selected item from available items
                 viewModel.AvailableItems.Remove(SelectedItem);
+                //set image
                 ItemPic.Source = EquippedItem.ImageURI;
             }
-
+            //set instruction label
             ItemDescLabel.Text = String.Format("{0} equipped, press Save to finalize and return", EquippedItem.Name);
         }
 
+        /// <summary>
+        /// unequip event handler, removes an equipped item from character, preparing for save clicked
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         public void UnequipButtonClicked(object sender, EventArgs e)
         {
+            //if no equipped item, do nothing, if eqipped item...
             if (EquippedItem != null)
             {
+                //add currently equipped item to available items
                 viewModel.AvailableItems.Add(EquippedItem);
+                //set instruction label
                 ItemDescLabel.Text = String.Format("{0} unequipped, press Save to finalize and return", EquippedItem.Name);
+                //set equipped item to null
                 EquippedItem = null;
+                //set image to default image
                 ItemPic.Source = "https://screenshotlayer.com/images/assets/placeholder.png";
             }
         }
 
-        private async void OnItemSelected(object sender, SelectedItemChangedEventArgs args)
+        /// <summary>
+        /// item selected handler
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        private void OnItemSelected(object sender, SelectedItemChangedEventArgs args)
         {
+            //if no selected item
             if (!(args.SelectedItem is Item item))
                 return;
-
+            //set labels describing selected item
             ItemDescLabel.Text = String.Format("{0}", item.Description);
             ItemAffectsLabel.Text = String.Format("This item affects {0} with value {1}", item.Attribute.ToString(), item.Value.ToString());
-
+            //set item image
             ItemPic.Source = item.ImageURI;
-
+            //set selected item
             SelectedItem = item;
         }
     }
